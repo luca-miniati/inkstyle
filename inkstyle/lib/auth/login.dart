ec2d3bc64992ec1d47f0c388e552fd2e5ce6e9b0
loo <luca.miniati@gmail.com>
2023-12-05 09:37:55 -0500

    idek anymore bro

--- inkstyle/lib/auth/login.dart
+++ inkstyle/lib/auth/login.dart
@@ -2,15 +2,59 @@ import 'package:flutter/cupertino.dart';
 
 
 class LoginPage extends StatefulWidget {
-    const LoginPage({super.key});
-    
-    @override
-    State<LoginPage> createState() => _LoginPageState();  
+  const LoginPage({super.key});
+  
+  @override
+  State<LoginPage> createState() => _LoginPageState();  
 }
 
 class _LoginPageState extends State<LoginPage> {
-    @override
-    Widget build(buildContext context) {
-        return 
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(
+        middle: Text('Login'),
+      ),
+      child: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CupertinoTextField(
+                placeholder: 'Email',
+                prefix: Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Icon(CupertinoIcons.person_solid),
+                ),
+                padding: EdgeInsets.all(12.0),
+                keyboardType: TextInputType.text,
+                autofocus: true,
+              ),
+              SizedBox(height: 16.0),
+              CupertinoTextField(
+                placeholder: 'Password',
+                prefix: Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Icon(CupertinoIcons.lock_fill),
+                ),
+                padding: EdgeInsets.all(12.0),
+                keyboardType: TextInputType.visiblePassword,
+                obscureText: true,
+              ),
+              SizedBox(height: 24.0),
+              CupertinoButton.filled(
+                child: Text('Login'),
+                onPressed: () {
+                  // Add your login logic here
+                  // For demonstration purposes, navigate to a new page
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
 
--- inkstyle/lib/auth/register.dart
+++ inkstyle/lib/auth/register.dart
@@ -1,1 +1,60 @@
 import 'package:flutter/cupertino.dart';
+
+
+class RegisterPage extends StatefulWidget {
+  const RegisterPage({super.key});
+  
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();  
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(
+        middle: Text('Register'),
+      ),
+      child: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CupertinoTextField(
+                placeholder: 'Email',
+                prefix: Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Icon(CupertinoIcons.person_solid),
+                ),
+                padding: EdgeInsets.all(12.0),
+                keyboardType: TextInputType.text,
+                autofocus: true,
+              ),
+              SizedBox(height: 16.0),
+              CupertinoTextField(
+                placeholder: 'Password',
+                prefix: Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Icon(CupertinoIcons.lock_fill),
+                ),
+                padding: EdgeInsets.all(12.0),
+                keyboardType: TextInputType.visiblePassword,
+                obscureText: true,
+              ),
+              SizedBox(height: 24.0),
+              CupertinoButton.filled(
+                child: Text('Register'),
+                onPressed: () {
+                  // Add your registration logic here
+                  // For demonstration purposes, navigate to a new page
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
